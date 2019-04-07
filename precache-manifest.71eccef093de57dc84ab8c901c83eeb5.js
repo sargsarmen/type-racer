0652fb754f7cdc2e40e84e679f6ce40c91f43d8c
Armen Sargsyan <sargsarmen@gmail.com>
2019-04-07 23:43:01 +0400

    Updates

---
+++ precache-manifest.71eccef093de57dc84ab8c901c83eeb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d9059cf9405ba03dc34",
+    "url": "/type-racer/static/js/runtime~main.4ca8b9d7.js"
+  },
+  {
+    "revision": "db60ce2bf126c36139ed",
+    "url": "/type-racer/static/js/main.4ddf07b3.chunk.js"
+  },
+  {
+    "revision": "071c501fe9c4a4925c9a",
+    "url": "/type-racer/static/js/2.88c78763.chunk.js"
+  },
+  {
+    "revision": "db60ce2bf126c36139ed",
+    "url": "/type-racer/static/css/main.cfd0a131.chunk.css"
+  },
+  {
+    "revision": "5ed89bae88cb8449ec2b51a2938ed124",
+    "url": "/type-racer/index.html"
+  }
+];
