59ec97be58b902c8fea82c5c4c8688d9c316e20c
Armen Sargsyan <sargsarmen@gmail.com>
2019-04-07 23:28:25 +0400

    publish

---
+++ docs/precache-manifest.5752d1f433027cf9e9f415cdd50fd9ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "db60ce2bf126c36139ed",
+    "url": "/static/js/main.4ddf07b3.chunk.js"
+  },
+  {
+    "revision": "fd5e4157eed1d509ce2f",
+    "url": "/static/js/2.99846c78.chunk.js"
+  },
+  {
+    "revision": "db60ce2bf126c36139ed",
+    "url": "/static/css/main.cfd0a131.chunk.css"
+  },
+  {
+    "revision": "9d2143615102980f28fcfb068358c9c0",
+    "url": "/index.html"
+  }
+];
